9865a00e8a1d9f67454905ef492ef88da6b2232b
RedArtelerist <fedorenko.max02@gmail.com>
2023-04-18 17:20:50 +0300

    some fixes

--- EcommerceStore/settings.py
+++ EcommerceStore/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'ecommerce-wayshop.herokuapp.com', 'wayshop.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://wayshop.up.railway.app','https://*.127.0.0.1', 'https://ecommerce-wayshop.herokuapp.com']
